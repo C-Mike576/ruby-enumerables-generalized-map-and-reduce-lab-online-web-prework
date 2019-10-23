8b2ebd2c465691f5ba362e383261aa3d5eaadeff
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 12:01:12 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,10 +3,10 @@ def map(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] ) 
+    new.push(source_array[i]) 
     i += 1
   end
-  yield new
+  yield (new)
 end
 
 def reduce (source_array, starting_point=0)
