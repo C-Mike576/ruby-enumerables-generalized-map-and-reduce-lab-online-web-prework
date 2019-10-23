bb54c198d26798bd77faac21780b3b09068f50c4
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 12:18:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,7 +1,12 @@
 # Your Code Here
 def map(source_array){block}
-  source_array do |new|
-    yield new
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( block ) 
+    i += 1
+  end
+  yield new
   end
 end
 
