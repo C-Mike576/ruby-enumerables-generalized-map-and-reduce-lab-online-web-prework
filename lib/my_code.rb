3b101925e021796779e49d3e99118742635b17ea
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 12:39:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,9 @@
 # Your Code Here
-def map(source_array)
-  yield 
+def map(&source_array)
+  new = []
+  self.each do |opperate|
+    new << source_array.call(opperate)
+  end
 end
 
 def reduce (source_array, starting_point=0)
