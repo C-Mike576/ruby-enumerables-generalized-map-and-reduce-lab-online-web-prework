84ac83c137a4547fb91e6fad8817d1adcba5108b
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 12:33:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -3,7 +3,7 @@ def map(source_array)
   yield new = []
   i = 0
   while i < source_array.length do
-    new << source_array[i] # <== Unique work
+    new << source_array[i]
     i += 1
   end
   return new
