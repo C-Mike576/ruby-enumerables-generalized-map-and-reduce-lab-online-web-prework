039a7633e9e9c68ab342a067cdf3f093e1d75239
Michael Calkins <m.calkins576@gmail.com>
2019-10-23 12:33:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,6 +1,12 @@
 # Your Code Here
 def map(source_array)
-  yield 
+  yield new = []
+  i = 0
+  while i < source_array.length do
+    new << source_array[i] # <== Unique work
+    i += 1
+  end
+  return new
 end
 
 def reduce (source_array, starting_point=0)
